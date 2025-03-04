ecd5381bd4b1289718c346e3d4c1c68a0d5ccced
Ben <ben.gimbel@okta.com>
2025-03-04 13:43:03 -0600

    fix logging

--- server/auth.js
+++ server/auth.js
@@ -4,6 +4,7 @@ const jwksRsa = require('jwks-rsa');
 const jwt = require('jsonwebtoken');
 const config = require('../lib/config');
 const registerSession = require('../lib/session');
+const logger = require('../lib/logger');
 
 const scopes = [{ value: 'openid' }, { value: 'profile' }];
 
@@ -39,6 +40,7 @@ module.exports = {
       complete: true,
       validate: async (decoded, req, callback) => {
         console.log(decoded, 'DECODED');
+        logger.info(decoded, 'decoded');
         if (!decoded) {
           return callback(null, false);
         }
@@ -53,6 +55,7 @@ module.exports = {
           ) {
             return jwtOptions.resourceServer.key(decoded, (keyErr, key) => {
               console.log(key, 'key', '1 if');
+              logger.info(key, 'key', '1 if');
               if (keyErr) {
                 return callback(Boom.boomify(keyErr), null, null);
               }
@@ -78,6 +81,7 @@ module.exports = {
             });
           } else if (decoded && decoded.payload && decoded.payload.iss === config('PUBLIC_WT_URL')) {
             console.log(token, 'token 2 if');
+            logger.info(token, 'token 2 if');
             return jwt.verify(
               token,
               jwtOptions.dashboardAdmin.key,
@@ -118,6 +122,7 @@ module.exports = {
         clientName: 'auth0-account-link',
         onLoginSuccess: (decoded, req, callback) => {
           console.log(decoded, 'on login success');
+          logger.info(decoded, 'on login success');
           if (decoded) {
             // eslint-disable-next-line no-param-reassign
             decoded.scope = scopes.map(scope => scope.value);
