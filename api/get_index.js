135bff58c8bd44e72326b51a675cd63c191f2ad0
Ben <ben.gimbel@okta.com>
2025-03-07 13:52:59 -0600

    async issue

--- api/get_index.js
+++ api/get_index.js
@@ -10,71 +10,71 @@ const getIdentityProviderPublicName = require('../lib/idProviders');
 const humanizeArray = require('../lib/humanize');
 const { resolveLocale } = require('../lib/locale');
 const { getSettings } = require('../lib/storage');
-const jwt = require('jsonwebtoken');
-const jwksRsa = require('jwks-rsa');
-const { promisify } = require('util');
+// const jwt = require('jsonwebtoken');
+// const jwksRsa = require('jwks-rsa');
+// const { promisify } = require('util');
 
-const jwtOptions = {
-  dashboardAdmin: {
-    key: config('EXTENSION_SECRET'),
-    verifyOptions: {
-      audience: 'urn:api-account-linking',
-      issuer: config('PUBLIC_WT_URL'),
-      algorithms: ['HS256']
-    }
-  },
-  resourceServer: {
-    key: jwksRsa.hapiJwt2KeyAsync({
-      cache: true,
-      rateLimit: true,
-      jwksRequestsPerMinute: 2,
-      jwksUri: `https://${config('AUTH0_DOMAIN')}/.well-known/jwks.json`
-    }),
-    verifyOptions: {
-      audience: 'urn:auth0-account-linking-api',
-      issuer: `https://${config('AUTH0_DOMAIN')}/`,
-      algorithms: ['RS256']
-    }
-  }
-};
-
-const handleJwt = async (token) => {
-  const getKeyAsync = promisify(jwtOptions.resourceServer.key);
-  const jwtVerifyAsync = promisify(jwt.verify);
-  try {
-    const resourceServerKey = await getKeyAsync(token);
-    await jwtVerifyAsync(
-      token,
-      resourceServerKey,
-      jwtOptions.resourceServer.verifyOptions
-    );
-  } catch (error) {
-    console.log('ERROR handlejwt', error);
-    logger.info('ERROR handlejwt', error);
-  }
-};
-
-// const decodeToken = token =>
-//   new Promise((resolve, reject) => {
-//     try {
-//       resolve(decode(token));
-//     } catch (e) {
-//       reject(e);
+// const jwtOptions = {
+//   dashboardAdmin: {
+//     key: config('EXTENSION_SECRET'),
+//     verifyOptions: {
+//       audience: 'urn:api-account-linking',
+//       issuer: config('PUBLIC_WT_URL'),
+//       algorithms: ['HS256']
 //     }
-//   });
+//   },
+//   resourceServer: {
+//     key: jwksRsa.hapiJwt2KeyAsync({
+//       cache: true,
+//       rateLimit: true,
+//       jwksRequestsPerMinute: 2,
+//       jwksUri: `https://${config('AUTH0_DOMAIN')}/.well-known/jwks.json`
+//     }),
+//     verifyOptions: {
+//       audience: 'urn:auth0-account-linking-api',
+//       issuer: `https://${config('AUTH0_DOMAIN')}/`,
+//       algorithms: ['RS256']
+//     }
+//   }
+// };
 
-const decodeToken = async (token) => {
-  try {
-    await handleJwt(token);
-    const decoded = decode(token);
-    logger.info('decoded', JSON.stringify(decoded));
-    console.log('decoded', JSON.stringify(decoded));
-    return decoded;
-  } catch (err) {
-    console.log('ERROR handlejwt decodeToken', err);
-    logger.info('ERROR handlejwt decodeToken', err);
-  }
-};
+// const handleJwt = async (token) => {
+//   const getKeyAsync = promisify(jwtOptions.resourceServer.key);
+//   const jwtVerifyAsync = promisify(jwt.verify);
+//   try {
+//     const resourceServerKey = await getKeyAsync(token);
+//     await jwtVerifyAsync(
+//       token,
+//       resourceServerKey,
+//       jwtOptions.resourceServer.verifyOptions
+//     );
+//   } catch (error) {
+//     console.log('ERROR handlejwt', error);
+//     logger.info('ERROR handlejwt', error);
+//   }
+// };
+
+const decodeToken = token =>
+  new Promise((resolve, reject) => {
+    try {
+      resolve(decode(token));
+    } catch (e) {
+      reject(e);
+    }
+  });
+
+// const decodeToken2 = async (token) => {
+//   try {
+//     await handleJwt(token);
+//     const decoded = decode(token);
+//     logger.info('decoded', JSON.stringify(decoded));
+//     console.log('decoded', JSON.stringify(decoded));
+//     return decoded;
+//   } catch (err) {
+//     console.log('ERROR handlejwt decodeToken', err);
+//     logger.info('ERROR handlejwt decodeToken', err);
+//   }
+// };
 
 const fetchUsersFromToken = ({ sub, email }) =>
   findUsersByEmail(email).then(users => ({
