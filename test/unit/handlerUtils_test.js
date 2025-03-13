3628a5a818c5bd8e47b967f7612e02a9861eb229
Ben <ben.gimbel@okta.com>
2025-03-13 15:13:43 -0500

    adding token validation failure tests

--- test/unit/handlerUtils_test.js
+++ test/unit/handlerUtils_test.js
@@ -1,6 +1,6 @@
 const { expect } = require('chai');
 const handlerUtils = require('../../lib/handlerUtils')
-const { createAuth0Token } = require('../test_helper');
+const { createAuth0Token, createWebtaskToken } = require('../test_helper');
 
 describe('Handler Utils Tests', () => {
     it('validates a token successfully', async () => {
@@ -13,4 +13,22 @@ describe('Handler Utils Tests', () => {
         expect(decoded.aud).to.deep.equal('22qApOBZ9BkEf3WDIYetEiJPXswpQdmY');
         expect(decoded.iss).to.deep.equal('https://account-linking-testing.auth0.com/');
     })
+    it('throws an error on validation for an a token not issued by auth0', async () => {
+        try {
+          const invalidToken = createWebtaskToken({ user_id: 'auth0|67d304a8b5dd1267e87c53ba', email: 'ben1@acme.com' });
+          await handlerUtils.validateAuth0Token(invalidToken);
+        } catch (error) {
+          expect(error).to.be.an('error');
+          expect(error.message).to.include('An error was encountered while decoding the token');
+        }
+    });
+    it('throws an error on validation for an a token that is not valid', async () => {
+        try {
+          const invalidToken = {iss: '123', aud: '123', sub: '123', email: '123'};
+          await handlerUtils.validateAuth0Token(invalidToken);
+        } catch (error) {
+          expect(error).to.be.an('error');
+          expect(error.message).to.include('An error was encountered while decoding the token');
+        }
+    });
 });
