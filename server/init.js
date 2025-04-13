22d69324fd4b9dfbc98ceeb2350c6acbe3240dbd
Ben <ben.gimbel@okta.com>
2025-04-13 12:56:51 -0500

    ran into issues, reverting init

--- server/init.js
+++ server/init.js
@@ -5,26 +5,28 @@ const createServer = require('./index');
 const logger = require('../lib/logger');
 const initStorage = require('../lib/db').init;
 
-const initServer = async (cfg, storageContext) => {
-  try {
-    // Set configuration provider.
-    config.setProvider(key => cfg(key) || process.env[key]);
-
-    // Initialize the storage context
-    initStorage(
-      storageContext
-        ? new WebtaskStorageContext(storageContext, { force: 1 })
-        : new FileStorageContext(path.join(__dirname, '../data.json'))
-    );
-    // Start the server.
-    await createServer();
-    logger.info('Hapi initialization completed.');
-  } catch (error) {
+const defaultCallback = (err) => {
+  if (err) {
     logger.error('Hapi initialization failed.');
-    logger.error(error);
-
-    throw error;
+    logger.error(err);
+  } else {
+    logger.info('Hapi initialization completed.');
   }
+};
+
+const initServer = async (cfg, storageContext, cb) => {
+  // Set configuration provider.
+  config.setProvider(key => cfg(key) || process.env[key]);
+
+  // Initialize the storage context
+  initStorage(
+    storageContext
+      ? new WebtaskStorageContext(storageContext, { force: 1 })
+      : new FileStorageContext(path.join(__dirname, '../data.json'))
+  );
+
+  // Start the server.
+  return createServer(cb || defaultCallback);
 };
 
 module.exports = initServer;
